b5428ece59a664bd7666880d4135a72809c84514
Shabthi <shabthij@gmail.com>
2019-05-16 15:54:57 +0530

    Update appRoute.js

--- Server/routes/appRoute.js
+++ Server/routes/appRoute.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 var Animal = require('../models/dataSchema');
 
+var Admin = require('../models/admin');
+
 router.post('/create', (req,  res,next) => {
     var newAnimal = new Animal({
         type: req.body.type,
@@ -50,6 +52,23 @@ router.delete('/delete/:id',(req,res,next)=> {
     }
     )
     res.status(200).json({msg:'Delete request workng'});
+});
+
+
+router.post('/admin/register', (req, res, next)=> {
+    var email = req.body.email;
+    var name = req.body.name;
+    var password = req.body.password;
+
+    Admin.create(email, name, password)
+    .then(function(result){
+        res.status(200);
+        res.send(result);
+    })
+    .catch(function(error){
+        res.status(500);
+        res.send(error);
+    });
 });
 
 module.exports = router;
