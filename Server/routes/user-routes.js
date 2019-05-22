8840c9ecec70f5a43d5242e699904154b92ab5b4
shehanrangana <shehanrnet@gmail.com>
2019-05-22 21:06:27 +0530

    User profile update function created(backend)

--- Server/routes/user-routes.js
+++ Server/routes/user-routes.js
@@ -2,11 +2,15 @@ const express = require('express');
 const router = express.Router();
 
 const userController = require('../controllers/user-controller');
+const checkAuth = require('../middleware/check-auth');
 
 // Signup
 router.post('/signup', userController.userSignup);
 
 // Login
 router.post('/login', userController.userLogin);
+
+// Update
+router.patch('/update/:userId', checkAuth, userController.userUpdate);
 
 module.exports = router;
