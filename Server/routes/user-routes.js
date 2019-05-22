e3c6edf4e068c900887ccd3bb4afd6ab227fded7
shehanrangana <shehanrnet@gmail.com>
2019-05-22 16:58:01 +0530

    User login created(backend)

--- Server/routes/user-routes.js
+++ Server/routes/user-routes.js
@@ -3,6 +3,10 @@ const router = express.Router();
 
 const userController = require('../controllers/user-controller');
 
+// Signup
 router.post('/signup', userController.userSignup);
+
+// Login
+router.post('/login', userController.userLogin);
 
 module.exports = router;
